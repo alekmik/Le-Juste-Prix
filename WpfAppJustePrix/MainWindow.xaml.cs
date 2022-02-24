59db4b9e5a1b8c538780b0dda35f80cd84254d84
Alek Mikolajek <alekmikopro@gmail.com>
2022-02-24 10:04:02 +0100

    The Right Number

---
+++ WpfAppJustePrix/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfAppJustePrix
+{
+    public partial class MainWindow : Window
+    {
+        Random randomNumber = new Random(); //generation of the random number
+        int mysteryNumber; //variable of the mystery number
+        int attemptUser = 1; //variable of the attemp of the user
+        public MainWindow()
+        {
+            InitializeComponent();
+            mysteryNumber = randomNumber.Next(1, 1001); // un number will be generated between 1 and 1000
+        }
+        private void buttonOK_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(userProposition.Text);
+            //answer to the proposal of the user
+            if (mysteryNumber < x)
+            {
+                MessageBox.Show("Votre proposition : " + x + ".\n" + "C'est moins !" + "\nNombre de tentative : " + "\n" + attemptUser);
+                userProposition.Text = ""; //Clear input
+            }
+            else if (mysteryNumber > x)
+            {
+                MessageBox.Show("Votre proposition : " + x + ".\n" + "C'est plus !" + "\nNombre de tentative : " + "\n" + attemptUser);
+                userProposition.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("BRAVO, vous avez gagné ! \nLa réponse est " + mysteryNumber +"\n\nNombre de tentative total : " + attemptUser + "\n\nRealized by Alek Mikolajek");
+                Environment.Exit(0);
+            }
+            //count the attempts
+            do
+            {
+                attemptUser++;
+            } while (x == mysteryNumber);
+        }
+    }
+}
